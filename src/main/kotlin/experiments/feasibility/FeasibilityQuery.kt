bbb77ebe7ca95297dd069a680c38c4ba5e94c7cf
knokko <knokogator@hotmail.com>
2025-03-17 09:29:01 +0100

    Code for parsing feasibility results

---
+++ src/main/kotlin/experiments/feasibility/FeasibilityQuery.kt
@@ -0,0 +1,104 @@
+package experiments.feasibility
+
+import java.io.File
+import java.util.*
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+fun main() {
+	val outerFolders = File("feasibility-results").listFiles()!!
+	val results = Collections.synchronizedList(ArrayList<FeasibilityCaseResult>())
+	val threadPool = Executors.newFixedThreadPool(8)
+	for (outerFolder in outerFolders) {
+		if (!outerFolder.isDirectory) continue
+		for (innerFolder in outerFolder.listFiles()!!) {
+			if (!innerFolder.isDirectory) continue
+			threadPool.submit {
+				results.add(FeasibilityCaseResult.parse(innerFolder))
+			}
+		}
+	}
+	threadPool.shutdown()
+	if (!threadPool.awaitTermination(5, TimeUnit.SECONDS)) throw Error("Timed out")
+
+	println("base stats: out of the ${results.size} problems:")
+	println("${results.count { it.certainlyFeasible }} are certainly feasible")
+	println("${results.count { it.certainlyInfeasible }} are certainly infeasible")
+	println("${results.count { !it.certainlyFeasible && !it.certainlyInfeasible }} are unsolved")
+	println()
+	for (numCores in 1 .. 5) {
+		val numProblems = results.count { !it.certainlyInfeasible && !it.certainlyFeasible && it.config.numCores == numCores }
+		println("  of which $numProblems have $numCores cores")
+	}
+	for (utilization in arrayOf(30, 70, 90, 100)) {
+		val numProblems = results.count { !it.certainlyInfeasible && !it.certainlyFeasible && it.config.utilization == utilization }
+		println("  of which $numProblems have $utilization% utilization")
+	}
+	for (numJobs in arrayOf(40, 80, 300, 1000, 5000)) {
+		val numProblems = results.count { !it.certainlyInfeasible && !it.certainlyFeasible && it.config.numJobs == numJobs }
+		println("  of which $numProblems have $numJobs jobs")
+	}
+	for (length in arrayOf(5, 20, 100, 5000)) {
+		val numProblems = results.count { !it.certainlyInfeasible && !it.certainlyFeasible && it.config.jobLength == length }
+		println("  of which $numProblems have an average job length of $length")
+	}
+	run {
+		val numProblems = results.count { !it.certainlyInfeasible && !it.certainlyFeasible && it.config.precedence }
+		println("  of which $numProblems have precedence constraints")
+	}
+
+	for (solver in arrayOf(
+		FeasibilityTest.HEURISTIC, FeasibilityTest.Z3_MODEL1, FeasibilityTest.CPLEX, FeasibilityTest.MINISAT
+	)) {
+		println()
+		if (solver == FeasibilityTest.MINISAT) {
+			println("out of the ${results.count { !it.config.precedence }} problems without precedence constraints:")
+		}
+		var relevantResults = results.map { it.getAll()[solver] }
+		if (solver == FeasibilityTest.MINISAT) relevantResults = relevantResults.filterNotNull()
+		println("The ${solver.name} test detected that ${relevantResults.count { it!!.certainlyFeasible }} problems are feasible")
+		println("The ${solver.name} test detected that ${relevantResults.count { it!!.certainlyInfeasible }} problems are infeasible")
+		val exclusiveFeasible = results.count {
+			val own = it.getAll()[solver] ?: return@count false
+			own.certainlyFeasible && !it.getAll().entries.any { inner -> inner.key != solver && inner.value.certainlyFeasible }
+		}
+		println("$exclusiveFeasible problems were only detected to be feasible by the ${solver.name} test")
+		val exclusiveInfeasible = results.count {
+			val own = it.getAll()[solver] ?: return@count false
+			own.certainlyInfeasible && !it.getAll().entries.any { inner -> inner.key != solver && inner.value.certainlyInfeasible }
+		}
+		println("$exclusiveInfeasible problems were only detected to be infeasible by the ${solver.name} test")
+		run {
+			val unsolved = relevantResults.count { !it!!.certainlyFeasible && !it.certainlyInfeasible }
+			println("$unsolved / ${relevantResults.size} problems were unsolved by the ${solver.name} test")
+		}
+		for (numCores in 1 .. 5) {
+			val unsolved = relevantResults.count { !it!!.certainlyInfeasible && !it.certainlyFeasible && it.config.numCores == numCores }
+			val total = relevantResults.count { it!!.config.numCores == numCores }
+			println("  $unsolved / $total problems with $numCores cores were unsolved")
+		}
+		for (utilization in arrayOf(30, 70, 90, 100)) {
+			val unsolved = relevantResults.count { !it!!.certainlyInfeasible && !it.certainlyFeasible && it.config.utilization == utilization }
+			val total = relevantResults.count { it!!.config.utilization == utilization }
+			println("  $unsolved / $total problems with $utilization% utilization were unsolved")
+			val unsolvedManyJobs = relevantResults.count { !it!!.certainlyInfeasible && !it.certainlyFeasible && it.config.utilization == utilization && it.config.numJobs == 5000 }
+			val totalManyJobs = relevantResults.count { it!!.config.utilization == utilization && it.config.numJobs == 5000 }
+			println("  $unsolvedManyJobs / $totalManyJobs problems with $utilization% utilization and 5000 jobs were unsolved")
+		}
+		for (numJobs in arrayOf(40, 80, 300, 1000, 5000)) {
+			val unsolved = relevantResults.count { !it!!.certainlyInfeasible && !it.certainlyFeasible && it.config.numJobs == numJobs }
+			val total = relevantResults.count { it!!.config.numJobs == numJobs }
+			println("  $unsolved / $total problems with $numJobs jobs were unsolved")
+		}
+		for (length in arrayOf(5, 20, 100, 5000)) {
+			val unsolved = relevantResults.count { !it!!.certainlyInfeasible && !it.certainlyFeasible && it.config.jobLength == length }
+			val total = relevantResults.count { it!!.config.jobLength == length }
+			println("  $unsolved / $total problems with an average job length of $length were unsolved")
+		}
+		run {
+			val unsolved = relevantResults.count { !it!!.certainlyInfeasible && !it.certainlyFeasible && it.config.precedence }
+			val total = relevantResults.count { it!!.config.precedence }
+			println("  $unsolved / $total problems with precedence constraints were unsolved")
+		}
+	}
+}
---
+++ src/main/kotlin/experiments/feasibility/FeasibilityResult.kt
@@ -0,0 +1,137 @@
+package experiments.feasibility
+
+import java.io.File
+import java.lang.Double.parseDouble
+import java.lang.Integer.parseInt
+import java.nio.file.Files
+
+class FeasibilitySolverResult(
+	val solver: FeasibilityTest,
+	val config: FeasibilityConfig,
+	val certainlyFeasible: Boolean,
+	val certainlyInfeasible: Boolean,
+	val spentSeconds: Double?,
+	val timedOut: Boolean,
+	val processTimedOut: Boolean,
+) {
+	companion object {
+		fun parse(solver: FeasibilityTest, config: FeasibilityConfig, file: File): FeasibilitySolverResult {
+			var certainlyFeasible = false
+			var certainlyInfeasible = false
+			var timedOut = false
+			var processTimedOut = false
+			var spentSeconds: Double? = null
+			for (line in Files.lines(file.toPath())) {
+				if (line.contains("is feasible.")) certainlyFeasible = true
+				if (line.contains("is infeasible")) certainlyInfeasible = true
+				if (line.contains("process timed out")) processTimedOut = true
+				else if (line.contains("timed out")) timedOut = true
+				val indexSeconds = line.indexOf(" seconds")
+				if (indexSeconds != -1) {
+					val startIndex = line.lastIndexOf(" ", indexSeconds - 1) + 1
+					spentSeconds = parseDouble(line.substring(startIndex, indexSeconds))
+				}
+			}
+
+			if (timedOut && spentSeconds != null) throw Error("Failed to parse $file")
+			return FeasibilitySolverResult(
+				solver,
+				config = config,
+				certainlyFeasible = certainlyFeasible,
+				certainlyInfeasible = certainlyInfeasible,
+				spentSeconds,
+				timedOut = timedOut,
+				processTimedOut = processTimedOut
+			)
+		}
+	}
+}
+
+class FeasibilityConfig(
+	val numJobs: Int,
+	val utilization: Int,
+	val numCores: Int,
+	val precedence: Boolean,
+	val jobLength: Int
+) {
+	companion object {
+		fun parse(folder: File): FeasibilityConfig {
+			val numJobs = run {
+				val endIndex = folder.path.indexOf("jobs")
+				val startIndex = folder.path.lastIndexOf("/", endIndex) + 1
+				parseInt(folder.path.substring(startIndex, endIndex))
+			}
+			val utilization = run {
+				val endIndex = folder.path.indexOf("util")
+				val startIndex = folder.path.lastIndexOf("_", endIndex) + 1
+				parseInt(folder.path.substring(startIndex, endIndex))
+			}
+			val numCores = run {
+				val endIndex = folder.path.indexOf("cores")
+				parseInt(folder.path.substring(endIndex - 1, endIndex))
+			}
+			val precedence = !folder.path.contains("no-prec")
+			val jobLength = run {
+				val endIndex = folder.path.indexOf("durations")
+				val startIndex = folder.path.lastIndexOf("_", endIndex) + 1
+				parseInt(folder.path.substring(startIndex, endIndex))
+			}
+			return FeasibilityConfig(
+				numJobs = numJobs, utilization = utilization, numCores = numCores,
+				precedence = precedence, jobLength = jobLength
+			)
+		}
+	}
+}
+
+class FeasibilityCaseResult(
+	val folder: File,
+	val config: FeasibilityConfig,
+	val heuristicResult: FeasibilitySolverResult,
+	val z3Result: FeasibilitySolverResult,
+	val cplexResult: FeasibilitySolverResult,
+	val minisatResult: FeasibilitySolverResult?
+) {
+	private val all = mutableMapOf<FeasibilityTest, FeasibilitySolverResult>()
+
+	init {
+		all[FeasibilityTest.HEURISTIC] = heuristicResult
+		all[FeasibilityTest.Z3_MODEL1] = z3Result
+		all[FeasibilityTest.CPLEX] = cplexResult
+		if (!config.precedence) all[FeasibilityTest.MINISAT] = minisatResult!!
+	}
+
+	fun getAll() = all
+
+	val certainlyFeasible = all.values.any { it.certainlyFeasible }
+	val certainlyInfeasible = all.values.any { it.certainlyInfeasible }
+
+	init {
+		if (certainlyFeasible && certainlyInfeasible) throw Error("Invalid result $folder")
+	}
+
+	companion object {
+		fun parse(folder: File): FeasibilityCaseResult {
+			val config = FeasibilityConfig.parse(folder)
+			return FeasibilityCaseResult(
+				folder = folder,
+				config = config,
+				heuristicResult = FeasibilitySolverResult.parse(
+					FeasibilityTest.HEURISTIC, config, File("$folder/results-heuristic.log")
+				),
+				z3Result = FeasibilitySolverResult.parse(
+					FeasibilityTest.Z3_MODEL1, config,
+					File("$folder/results-z3-model1.log")
+				),
+				cplexResult = FeasibilitySolverResult.parse(
+					FeasibilityTest.CPLEX, config,
+					File("$folder/results-cplex.log")
+				),
+				minisatResult = if (config.precedence) null else FeasibilitySolverResult.parse(
+					FeasibilityTest.MINISAT, config,
+					File("$folder/results-minisat.log")
+				)
+			)
+		}
+	}
+}
